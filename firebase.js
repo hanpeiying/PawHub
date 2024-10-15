109877c43c236aa7fd76b8d83134c8555d04c156
Han PeiYing <hanpeiyingggg@gmail.com>
2024-10-16 00:15:46 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.API_KEY,
+  apiKey: "AIzaSyApb-p_DzaRgN2FGcm6X7kXLHJIGQvmoOw",
   authDomain: "pawhub-858e8.firebaseapp.com",
   projectId: "pawhub-858e8",
   storageBucket: "pawhub-858e8.appspot.com",
