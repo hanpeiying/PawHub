d3ea56a7cb79beffe32c732824b481c43b707ae3
CLY246 <leyong656@gmail.com>
2024-10-22 13:39:46 +0800

    hide the firebase config

--- firebase.js
+++ firebase.js
@@ -1,19 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-
+import { firebaseConfig } from "./configure.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyApb-p_DzaRgN2FGcm6X7kXLHJIGQvmoOw",
-  authDomain: "pawhub-858e8.firebaseapp.com",
-  projectId: "pawhub-858e8",
-  storageBucket: "pawhub-858e8.appspot.com",
-  messagingSenderId: "1064105879369",
-  appId: "1:1064105879369:web:19229d362355cbdfa11c98",
-  measurementId: "G-4HCFE4PC1Q",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
