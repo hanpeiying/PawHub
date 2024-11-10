0626b5edcfde704d40f02aca52cbdf09e4be9133
CLY246 <leyong656@gmail.com>
2024-11-11 00:06:39 +0800

    more updates

--- public/firebase.js
+++ public/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { firebaseConfig } from "../configure.js";
+import { firebaseConfig } from "./configure.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
